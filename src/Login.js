40ee48b7e533f64233ac9b7b7ef827d5889bf7e9
ShivS01 <shivanshusingh10@yahoo.in>
2021-05-20 08:41:03 +0530

    Adding formatting, login/form

---
+++ src/Login.js
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import Form from "./components/Form";
+
+const Login = () => {
+  const [user, setUser] = useState("");
+  const [pass, setPass] = useState("");
+  const [login, setLogin] = useState(false);
+  const updateUser = (event) => setUser(event.target.value);
+  const updatePass = (event) => setPass(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    const data = {
+      username: user,
+      password: pass,
+    };
+    console.log(data);
+    const sendToServer = JSON.stringify(data);
+    console.log(`JSON String sent to server: ${sendToServer}`);
+    setLogin(true);
+    setUser("");
+    setPass("");
+  };
+  if (login !== null) {
+    setTimeout(() => {
+      setLogin(null);
+    }, 5000);
+  }
+  return (
+    <>
+      <h1>Login</h1>
+      {login ? <div className={"success"}>Logged in</div> : null}
+      <Form
+        username={user}
+        password={pass}
+        handleUser={updateUser}
+        handlePass={updatePass}
+        handleSubmit={onSubmit}
+      />
+    </>
+  );
+};
+
+export default Login;
---
+++ src/components/Form.js
@@ -0,0 +1,19 @@
+const Form = ({ username, password, handleUser, handlePass, handleSubmit }) => {
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>Username:</label>
+        <input type="text" value={username} onChange={handleUser} />
+      </div>
+      <div>
+        <label>Password:</label>
+        <input type="password" value={password} onChange={handlePass} />
+      </div>
+      <div>
+        <button type="submit">Login</button>
+      </div>
+    </form>
+  );
+};
+
+export default Form;
