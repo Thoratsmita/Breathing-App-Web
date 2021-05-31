a38cffa2a3609230a6a4680dc29d9152a60f0839
ShivS01 <shivanshusingh10@yahoo.in>
2021-05-31 14:36:31 +0530

    cleaning services code

--- src/services/Data.js
+++ src/services/Data.js
@@ -1,22 +1,23 @@
 import Axios from "axios";
-const baseUrl = "http://localhost:3001/usersData";
-const citiUrl = "http://localhost:3001/cities";
+const baseUrl = "http://localhost:3001";
 
-const getAll = () => Axios.get(baseUrl).then((response) => response.data);
-const getAll = () => Axios.get(citiUrl).then((response) => response.data);
+const getAll = (path) =>
+  Axios.get(`${baseUrl}/${path}`).then((response) => response.data);
 
-const create = (newObj) =>
-  Axios.post(baseUrl, newObj).then((response) => response.data);
+// const create = (newObj) =>
+//   Axios.post(baseUrl, newObj).then((response) => response.data);
 
-const update = (id, updatedObj) =>
-  Axios.put(`${baseUrl}/${id}`, updatedObj).then((response) => {
-    console.log(response);
-  });
+// const update = (id, updatedObj) =>
+//   Axios.put(`${baseUrl}/${id}`, updatedObj).then((response) => {
+//     console.log(response);
+//   });
 
-const remove = (id) =>
-  Axios.delete(`${baseUrl}/${id}`).then((response) => {
-    console.log(response);
-    return response.data;
-  });
+// const remove = (id) =>
+//   Axios.delete(`${baseUrl}/${id}`).then((response) => {
+//     console.log(response);
+//     return response.data;
+//   });
 
-export default { getAll, create, update, remove };
+const DataService = { getAll };
+
+export default DataService;
