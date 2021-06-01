1d7c9a8d6d14f6dcaa8ed6ae87a9e9f777438850
ShivS01 <shivanshusingh10@yahoo.in>
2021-06-01 18:40:14 +0530

    Updated Services Page

--- src/Services.js
+++ src/Services.js
@@ -1,22 +1,28 @@
 import React, { useEffect, useState } from "react";
-import { Container, Grid, makeStyles, Paper } from "@material-ui/core";
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Grid,
+  makeStyles,
+  Typography,
+} from "@material-ui/core";
 import DataService from "./services/Data";
+import realEstate from "./assets/real-estate.jpg";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: "center",
-    color: theme.palette.text.secondary,
-  },
+  card: { maxWidth: 350, padding: theme.spacing(2) },
 }));
 
 const Services = () => {
   const classes = useStyles();
+  const [services, setServices] = useState([]);
 
-  const [services, setServices] = useState("");
   useEffect(() => {
     DataService.getAll("servicesData").then((response) =>
       setServices(response)
@@ -25,16 +31,29 @@ const Services = () => {
 
   console.log(services);
   return (
-    <>
+    <div style={{ paddingLeft: 200, paddingRight: 200 }}>
       <div>Services</div>
-      <Container>
-        <Grid container>
-          <Grid item xs={6}>
-            <Paper>brr</Paper>
+      <Grid container className={classes.root} spacing={4}>
+        {services.map((service, index) => (
+          <Grid key={index} item xs={6}>
+            <Card className={classes.card}>
+              <CardMedia image={realEstate} />
+              <CardContent>
+                <Typography gutterBottom>{service.title}</Typography>
+                <Typography color="textSecondary">
+                  {service.description}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button color="primary" variant="contained">
+                  More Details
+                </Button>
+              </CardActions>
+            </Card>
           </Grid>
-        </Grid>
-      </Container>
-    </>
+        ))}
+      </Grid>
+    </div>
   );
 };
 
