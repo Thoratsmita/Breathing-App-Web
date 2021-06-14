85ebabd772d5f591c33170e4ea820465dbfa923e
Madhura Mhatre <madhuramhatrejm@gmail.com>
2021-06-15 00:27:19 +0530

    added a counter

--- src/Geofence.js
+++ src/Geofence.js
@@ -1,37 +1,96 @@
-import React, { useState, useEffect } from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import React, { useState } from "react";
+import FormGroup from "@material-ui/core/FormGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Switch from "@material-ui/core/Switch";
 import Button from "@material-ui/core/Button";
-import Registermain from "./Registermain";
-import logo from "./assets/logo.png";
-import Grid from "@material-ui/core/Grid";
 import TextField from "@material-ui/core/TextField";
-import Loginmain from "./Loginmain";
-import "./login.css";
+import DataService from "./service/Data";
 
+import { makeStyles } from "@material-ui/core/styles";
 const Geofence = () => {
+  const [state, setState] = React.useState({
+    checkedA: true,
+    checkedB: true,
+  });
+  const [count, setCount] = useState(0);
+  function increase() {
+    setCount(count + 1);
+  }
+  function decrease() {
+    setCount(count - 1);
+  }
+  function input(event) {
+    setCount({ count: event.target.value });
+  }
+  const handleChange = (event) => {
+    setState({ ...state, [event.target.name]: event.target.checked });
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+    const data = {
+      count,
+    };
+    console.log(data);
+    DataService.create("geofencdata", data);
+    console.log(`Data sent to server: ${JSON.stringify(data)}`);
+  };
   const useStyles = makeStyles((theme) => ({
     root: {
-      "& > *": {
-        width: "25ch",
-        wrap: "flex",
-        overflow: "hidden",
-      },
+      flexGrow: 1,
     },
-
-    image: {
-      margin: "0px",
-      width: "100%",
-      height: "100vh",
-      flexGrow: "1",
-      display: "flex",
+    counter: {
+      background: " #eec311",
+      padding: "10px",
+      width: "64px",
+      height: "54px",
+      fontSize: "20px",
+      color: "#fffff",
+    },
+    save: {
+      width: "179px",
+      height: "49px",
+      margin: "3px 15px 11px 118px",
+      padding: "14px 65px 13px 66px",
+      borderRadius: "34.5px",
+      backgroundColor: "#4de382",
+      justifyContent: "right",
+      color: "#ffffff",
     },
   }));
   const classes = useStyles();
   return (
-    <div>
-      <h1>Components yet to be added</h1>
-    </div>
+    <FormGroup row>
+      <FormControlLabel
+        control={
+          <Switch
+            checked={state.checkedB}
+            onChange={handleChange}
+            name="checkedB"
+            color="primary"
+          />
+        }
+      />
+      <Button
+        variant="contained"
+        className={classes.counter}
+        onClick={decrease}
+      >
+        -
+      </Button>
+      <TextField id="outlined-basic" variant="outlined" value={count} />
+
+      <Button
+        onClick={increase}
+        variant="contained"
+        className={classes.counter}
+      >
+        +
+      </Button>
+
+      <Button onClick={onSubmit} variant="contained" className={classes.save}>
+        Save
+      </Button>
+    </FormGroup>
   );
 };
-
 export default Geofence;
