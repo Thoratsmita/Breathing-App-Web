1367daf7fac8a34bfecbe9371a636647359c35ff
aastha432 <aasthasarkar13gul@gmail.com>
2021-08-05 17:46:41 +0530

    Added toggling background effect to Sound Alarm

--- src/SoundAlarm.js
+++ src/SoundAlarm.js
@@ -7,6 +7,14 @@ import { makeStyles } from "@material-ui/core/styles";
 import "./login.css";
 
 const SoundAlarm = () => {
+
+  const [soundAlarm, setSoundAlarm] = React.useState(false);
+
+  const handleSoundAlarm = () => {
+    soundAlarm ? setSoundAlarm(false) : setSoundAlarm(true);
+  }
+
+
   const useStyles = makeStyles((theme) => ({
     root: {
       "& > *": {
@@ -21,19 +29,27 @@ const SoundAlarm = () => {
 
       zIndex: "1",
     },
-    image2: {
+    image2on: {
       marginTop: "70%",
       position: "relative",
       zIndex: "1",
+      background: "radial-gradient(circle, green, rgb(34, 240, 7),rgb(132, 245, 118), white)"
     },
+    image2off: {
+      marginTop: "70%",
+      position: "relative",
+      zIndex: "1",
+      background: "radial-gradient(circle, red, rgb(227, 20, 20),rgb(245, 66, 66), white)"
+    }
+  
   }));
   const classes = useStyles();
   return (
     <div>
       <div>
         <center>
-          <Button>
-            <img src={Soundalarm} className={classes.image2} />
+          <Button onClick={handleSoundAlarm}>
+            <img src={Soundalarm} className={ soundAlarm ? classes.image2on : classes.image2off} />
           </Button>
           <br></br>
           <Typography variant="h5" gutterBottom>
